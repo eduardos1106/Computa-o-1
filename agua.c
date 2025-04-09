f4bb3e4a2a3179916379b7c9946c23d6b722ee76
eduardos1106 <eduardosegatto@alunos.utfpr.edu.br>
2025-04-09 09:35:10 -0300

    Add files via upload

---
+++ agua.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese"); //a conta eh definida por f(x)=7+ x.valor que depende do quanto usou
+
+    int taxa = 7;
+    int metros, conta1, conta2, conta3;
+    printf("Digite qual foi a quantidade de água (em metros cúbicos) que foi utilizado no mês: ");
+    scanf("%d", &metros);
+
+    conta1 = metros*1;
+    conta2 = metros*2;
+    conta3 = metros*5;
+
+    if ((metros>0&&metros<=10)||(metros==0))
+    {
+        printf("O valor a ser pago é: %d$", taxa);
+    }
+    else if (metros>10&&metros<=30)
+    {
+        printf("O valor a ser pago é: %d$", conta1);
+    }
+
+    else if (metros>30&&metros<=100)
+    {
+        printf("O valor a ser pago é: %d$", conta2);
+    }
+
+    else
+    {
+        printf("O valor a ser pago é: %d$", conta3);
+    }
+
+    printf("\nAdeus!");
+    return 0;
+}
