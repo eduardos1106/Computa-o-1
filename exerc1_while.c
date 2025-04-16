baa6a9b694fcae3b2eddfeb109e2f65f5ab87faa
eduardos1106 <eduardosegatto@alunos.utfpr.edu.br>
2025-04-16 10:08:41 -0300

    Update exerc1_while.c

--- exerc1_while.c
+++ exerc1_while.c
@@ -3,22 +3,22 @@
 int main()
 {
 
-    int n, teste=0;
+    int n, maior=0;
     printf("Digite um número inteiro positivo (caso for negativo, irá encerrar o programa): ");
     scanf("%d", &n);
 
     while (n>0)
     {
-        if (n>teste)
+        if (n>maior)
         {
 
-            teste=n;
+            maior=n;
         }
 
         printf("\nDigite outro valor seguindo as mesmas regras: ");
         scanf("%d", &n);
     }
-    printf("\nO maior valor digitado foi: %d", teste);
+    printf("\nO maior valor digitado foi: %d", maior);
 
     printf("\nAdeus");
     return 0;
