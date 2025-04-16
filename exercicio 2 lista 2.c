79a04d7f4d34318ddc2a6de40a6a7f888accf30b
eduardos1106 <eduardosegatto@alunos.utfpr.edu.br>
2025-04-16 09:57:22 -0300

    Update exercicio 2 lista 2.c

--- exercicio 2 lista 2.c
+++ exercicio 2 lista 2.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-int n;
-float contagem, soma=0;
+    int n;
+    float soma, contagem;
 
-printf("Digite um valor inteiro maior que -1000 (caso contrario vai quebrar): ");
-scanf("%d", &n);
-
-while (n>-1000){
-
-if (n>0){
-    soma = soma +n;
-    contagem++;
-}
-
-    printf("\nDigite outro valor inteiro maior que -1000: ");
+    printf("Digite um valor inteiro maior que -1000 (caso contrario vai quebrar): ");
     scanf("%d", &n);
 
-}
-printf("\nA quantidade de valores positivos digitados foi: %.1f", contagem);
-printf("\nA média dos números positivos eh: %.2f", soma/contagem);
+    while (n>-1000)
+    {
 
-printf("\nAdeus");
-return 0;
+        if (n>0)
+        {
+            soma = soma +n;
+            contagem++;
+        }
+
+        printf("\nDigite outro valor inteiro maior que -1000: ");
+        scanf("%d", &n);
+
+    }
+    printf("\nA quantidade de valores positivos digitados foi: %.1f", contagem);
+    printf("\nA média dos números positivos eh: %.2f", soma/contagem);
+
+    printf("\nAdeus");
+    return 0;
 }
