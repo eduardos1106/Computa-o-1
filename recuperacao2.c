67bf9a3d3a720066a1b87998d74dd3221ddd48a5
eduardos1106 <eduardosegatto@alunos.utfpr.edu.br>
2025-04-23 08:40:57 -0300

    Update recuperacao2.c

--- recuperacao2.c
+++ recuperacao2.c
@@ -5,19 +5,22 @@ int main()
 {
     setlocale(LC_ALL, "Portuguese");//dado um numero, calcular a soma de seus multiplos entre 1 e 10
 
-    int n, conta=0;
-    int a=1;
+    int n, a1, a2, conta=0;
     printf("Digite um número para calcular a soma de seus múltiplos: ");
     scanf("%d", &n);
+    printf("Digite o intervalo inicial o qual quer calcular seus múltiplos: ");
+    scanf("%d", &a1);
+    printf("Digite o intervalo final o qual quer calcular seus múltiplos: ");
+    scanf("%d", &a2);
 
-    for(int i=0; i<=10; i++)
+    for(int i=a1; i<=a2; i++)
     {
-        if (n*a>10)
+        if (n*a1>a2)
         {
             break;
         }
-        conta = conta + n * a;
-        a++;
+        conta = conta + n * a1;
+        a1++;
     }
     printf("\nA soma de seus múltiplos no intervalo é: %d", conta);
     printf("\nFim do programa");
