853e4af94c99c55533e445fa2c3551935283edc7
eduardos1106 <eduardosegatto@alunos.utfpr.edu.br>
2025-05-14 08:28:48 -0300

    Update função que converte seg total para HH:MM:SS.c

--- função que converte seg total para HH:MM:SS.c
+++ função que converte seg total para HH:MM:SS.c
@@ -1,40 +1,23 @@
 #include <stdio.h>
+#include <locale.h>
 
-void converte(int segtotal, int *horas, int *min, int *seg)
+void converte(int segtotal,int *horas, int *min, int *seg)
 {
-
-    if (segtotal>=3600)
-    {
-        *horas = segtotal/3600;
-        segtotal = segtotal - *horas*3600;
-        if (segtotal==0){
-            *min==0;
-        *seg==0;
-        }
-        *min = segtotal/60;
-        *seg = segtotal - *min*60;
-    }
-    else
-    {
-        *horas=0;
-        *min = segtotal/60;
-        if(segtotal==0){
-            *seg==0;
-        }
-        *seg = segtotal - *min*60;
-    }
+    *horas = segtotal/3600;
+    segtotal = segtotal - *horas*3600;
+    *min = segtotal/60;
+    *seg = segtotal - *min*60;
 }
 
 int main()
 {
-    int segtotal;
-    int seg;
-    int min, horas;
+    setlocale(LC_ALL, "Portuguese");
+    int segtotal, horas, min, seg;
 
-    printf("Digite a quantidade total de segundos para converter: ");
+    printf("Insera o total de segundos para converter: ");
     scanf("%d", &segtotal);
 
-    converte(segtotal, &horas, &min, &seg);
-    printf("\nA quantidade de segundos: %d, convertida eh: %02d:%02d:%02d", segtotal, horas, min, seg);
+    converte(segtotal,&horas,&min,&seg);
+    printf("%d segundos convertido é: %02d:%02d:%02d", segtotal, horas, min,seg);
     return 0;
 }
